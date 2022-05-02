d67f30d3797cb901c165dffd6a10937930ad6aa2
afikri <fikri.abdul@gmx.com>
2022-05-02 11:07:58 +0700

    Adding BirthdayCakeCandles solution

--- Algorithms/BirthdayCakeCandles/Solution.java
+++ Algorithms/BirthdayCakeCandles/Solution.java
@@ -1,24 +1,29 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        Integer[] candlesArr = { 3, 2, 1, 3 };
+        Integer[] candlesArr = { 3, 2, 1, 3};
         List<Integer> tallestCandlesArr = Arrays.asList(candlesArr);
         birthdayCakeCandles(tallestCandlesArr);
     }
 
     public static int birthdayCakeCandles(List<Integer> candles) {
         // Write your code here
-        int max = candles.get(0);
-        int ctrMax = 0;
-        for (int i = 0; i < candles.size(); i++) {
-            if (candles.get(i) > max) {
-                max = candles.get(i);
-                ctrMax++;
+        // 1. Sort
+        Collections.sort(candles);
+        System.out.println(candles);
+
+        // count the duplicate of the last element
+        int max = 0;
+        int lastElem = candles.get(candles.size()-1);
+        for(int i = 0; i<candles.size();i++){
+            if (candles.get(i)== lastElem) {
+                max++;
             }
         }
-        System.out.println(ctrMax);
-        return 1;
+        System.out.println(max);
+        return max;
     }
 }
